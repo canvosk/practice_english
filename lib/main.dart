92666fbd06f692a5d7a0f1316eb4b055865adc8e
canvosk <bellibasllarcn@gmail.com>
2021-12-24 15:28:00 +0300

    first upload

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Practice English',
+      home: Iskele(),
+    );
+  }
+}
+
+class Iskele extends StatelessWidget {
+  const Iskele({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Practice English",
+        ),
+      ),
+      body: AnaEkran(),
+    );
+  }
+}
+
+class AnaEkran extends StatefulWidget {
+  const AnaEkran({Key? key}) : super(key: key);
+
+  @override
+  _AnaEkranState createState() => _AnaEkranState();
+}
+
+class _AnaEkranState extends State<AnaEkran> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("KELİMELER BURAYA GELECEK"),
+    );
+  }
+}
